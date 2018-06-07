2fd2c345295744c26b1e5788e0b9f4b39267b239
Shiratie Prodhan <shiratiee@gmail.com>
2018-06-07 17:11:17 -0400

    forgot about sqlize associations whoops.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,7 +7,8 @@ const Match = require('./match')
  *
  *    BlogPost.belongsTo(User)
  */
-
+User.hasMany(Match, {onDelete: "cascade"})
+Match.belongsTo(User)
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
