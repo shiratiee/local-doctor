09eea9a213052516182e08db0f1ce49ba670aa8a
Shiratie Prodhan <shiratiee@gmail.com>
2018-06-07 16:46:53 -0400

    combining reducers

--- client/store/index.js
+++ client/store/index.js
@@ -4,17 +4,34 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import doctors from './doctors'
+import matchdocs from './matchdocs'
+import matches from './matches'
 
 
-const reducer = combineReducers({user, doctors})
+const reducer = combineReducers({user, doctors, matchdocs, matches})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
 ))
-const store = createStore(reducer, middleware)
+
+
+const persistedState = localStorage.getItem('store') ? JSON.parse(localStorage.getItem('store')) : {};
+
+const rootReducer = (state, action) => {
+  if (action.type === 'LOGOUT_USER') {
+    state = undefined;
+  }
+  return reducer(state, action)
+}
+
+const store = createStore(rootReducer, persistedState, middleware);
+
+store.subscribe(() => localStorage.setItem('store', JSON.stringify(store.getState())));
 
 export default store
 export * from './user'
 export * from './doctors'
+export * from './matchdocs'
+export * from './matches'
 
 
