c4a97cd847798d43a2eae96ebaa26fa893784a5f
Shiratie Prodhan <shiratiee@gmail.com>
2018-06-20 20:34:28 -0400

    bye unique key error

--- client/components/Matches.js
+++ client/components/Matches.js
@@ -13,10 +13,10 @@ class Matches extends Component {
         <h1>Matches</h1>
         <div className="matchesList">
           {this.props.matches.length ?
-              this.props.matchdocs.map(data=> { 
+              this.props.matchdocs.map((data) => { 
                 console.log(this.props.matchdocs, "THISSSS")
                 return ( 
-                <div key={data.uid} className="matches docCard">
+                <div key={`${data.uid}`} className="matches docCard">
                 <CardStack
                 height={500}
                 width={280}
@@ -24,37 +24,41 @@ class Matches extends Component {
                 hoverOffset={25}>
 
                 <Card background='#c7b1c7'>
-                <h3 style={{ textDecoration: 'underline' }}> {data.profile.first_name}
-                {data.profile.last_name}, {data.profile.title} </h3> 
-                <img className="doc-image" src={data.profile.image_url} />
+                <h3 style={{ textDecoration: 'underline' }}> {data.data.profile.first_name}
+                {data.data.profile.last_name}, {data.data.profile.title} </h3> 
+                <img className="doc-image" src={data.data.profile.image_url} />
                 <br></br>
                 <br></br>
                 <span style={{ textDecoration: 'underline' }}> Address </span>
                 <p>
-                  {data.practices[0].visit_address.street} <br></br>
-                  {data.practices[0].visit_address.city}, {data.practices[0].visit_address.state} <br></br>
+                  {data.data.practices[0].visit_address.street} <br></br>
+                  {data.data.practices[0].visit_address.city}, {data.data.practices[0].visit_address.state} <br></br>
                 </p> 
                 <span style={{ textDecoration: 'underline' }}>Phone Number </span> 
-                {data.practices[0].phones.length ?
-                   <p>{data.practices[0].phones[0].number}</p>
+                {data.data.practices[0].phones.length ?
+                   <p>{data.data.practices[0].phones[0].number}</p>
                    : <p>None Provided</p>}
               <span style={{ textDecoration: 'underline' }}>Website </span> 
-                {data.practices[0].website ? 
-               <p> <a href={data.practices[0].website}> Click here for website</a></p>
+                {data.data.practices[0].website ? 
+               <p> <a href={data.data.practices[0].website}> Click here for website</a></p>
                 : <p>None Provided</p>}
                 </Card>
 
                 <Card background='#8b9dc3'>
                 <h3 style={{ textDecoration: 'underline' }}> Accepted Insurances </h3>
                 
-                {data.insurances.length 
-                  ? data.insurances.map(insurance => insurance.insurance_provider.name).join(', ').slice(0,790)+"..."
+                {data.data.insurances.length 
+                  ? data.data.insurances.map(insurance => insurance.insurance_provider.name).join(', ').slice(0,790)+"..."
                   : <span>No insurance listed. Contact doctor for more information.</span> }
                 
                 </Card>
                 </CardStack>    
                 </div>
-                )})
+                
+                )
+
+              })
+                
               : <p>NO MATCHES!</p>
           }
         </div>
--- client/store/matchdocs.js
+++ client/store/matchdocs.js
@@ -21,9 +21,8 @@ const removedUnmatchData = () => ({
 
 export const fetchDocById = (docId) =>
   dispatch => {
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors/${docId}?user_key=6ffaf2f592ca4029cf614bb4bf313be5`,
+    axios.get(`https://cors-anywhere.herokuapp.com/https://api.betterdoctor.com/2016-03-01/doctors/${docId}?user_key=6ffaf2f592ca4029cf614bb4bf313be5`,
     {headers: {'Access-Control-Allow-Origin': '*'}})
-    .then(res => res.json())
       .then((res) => {
         dispatch(fetchOneDocById(res.data));
       })
