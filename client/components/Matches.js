88f357216ab0db41392f340ddccca8afbed144eb
Shiratie Prodhan <shiratiee@gmail.com>
2018-08-07 13:24:08 -0400

    cleaning up matches component

--- client/components/Matches.js
+++ client/components/Matches.js
@@ -10,80 +10,80 @@ class Matches extends Component {
     return (
       <div>
         <h2 className="matches">Matches</h2>
-        <div className="row">
-          {this.props.matches.length ?
+          <div className="row">
+            {this.props.matches.length ?
               this.props.matches.map((data, i) => { 
                 console.log(this.props.matches, "THISSSS")
                 return ( 
                 <div key={i}>
-                <ul className="doctor">
-                <li>
-                <CardStack
-                height={500}
-                width={280}
-                background='#c7b1c7'
-                hoverOffset={25}>
+                  <ul className="doctor">
+                    <li>
+                      <CardStack
+                      height={500}
+                      width={280}
+                      background='#c7b1c7'
+                      hoverOffset={25}
+                      >
+                        <Card background='#c7b1c7'>
+                        <button	
+                          onClick={(event) => {
+                            event.preventDefault(); 
+                            this.props.onUnmatch(
+                            this.props.matches.id, 
+                            this.props.user.id,
+                            this.props.matches.firstName,
+                            this.props.matches.lastName,
+                            this.props.matches.title,
+                            this.props.matches.image_url,
+                            this.props.matches.street,
+                            this.props.matches.city,
+                            this.props.matches.state,
+                            this.props.matches.phoneNum,
+                            this.props.matches.website
+                            );
+                          }}
+                          >	
+                          X	
+                        </button>
+                        <h3 style={{ textDecoration: 'underline' }}> 
+                        {this.props.matches[0].firstName}
+                        {this.props.matches[0].lastName}, {this.props.matches[0].title} 
+                        </h3> 
+                        <img className="doc-image" src={this.props.matches[0].image_url} />
+                        <br></br>
+                        <br></br>
+                        <span style={{ textDecoration: 'underline' }}>Address</span>
+                        <p>
+                          {this.props.matches[0].street} <br></br>
+                          {this.props.matches[0].city}, {this.props.matches[0].state}<br></br>
+                        </p> 
+                        <span style={{ textDecoration: 'underline' }}>Phone Number</span> 
+                        {this.props.matches[0].phoneNum 
+                          ? <p>{this.props.matches[0].phoneNum}</p>
+                          : <p>None Provided</p>}
+                      <span style={{ textDecoration: 'underline' }}>Website </span> 
+                        {this.props.matches[0].website 
+                          ? <p> 
+                              <a href={this.props.matches[0].website}> Click here for website</a>
+                            </p>
+                          : <p>None Provided</p>}
+                        </Card>
 
-                <Card background='#c7b1c7'>
-                <button	
-                onClick={(event) => {
-                  event.preventDefault(); 
-                  this.props.onUnmatch(
-                  this.props.matches.id, 
-                  this.props.user.id,
-                  this.props.matches.firstName,
-                  this.props.matches.lastName,
-                  this.props.matches.title,
-                  this.props.matches.image_url,
-                  this.props.matches.street,
-                  this.props.matches.city,
-                  this.props.matches.state,
-                  this.props.matches.phoneNum,
-                  this.props.matches.website
-                  );
-                }}
-                    >	
-                    X	
-                    </button>
-                <h3 style={{ textDecoration: 'underline' }}> {this.props.matches[0].firstName}
-                {this.props.matches[0].lastName}, {this.props.matches[0].title} </h3> 
-                <img className="doc-image" src={this.props.matches[0].image_url} />
-                <br></br>
-                <br></br>
-                <span style={{ textDecoration: 'underline' }}> Address </span>
-                <p>
-                  {this.props.matches[0].street} <br></br>
-                  {this.props.matches[0].city}, {this.props.matches[0].state} <br></br>
-                </p> 
-                <span style={{ textDecoration: 'underline' }}>Phone Number </span> 
-                {this.props.matches[0].phoneNum ?
-                   <p>{this.props.matches[0].phoneNum}</p>
-                   : <p>None Provided</p>}
-              <span style={{ textDecoration: 'underline' }}>Website </span> 
-                {this.props.matches[0].website ? 
-               <p> <a href={this.props.matches[0].website}> Click here for website</a></p>
-                : <p>None Provided</p>}
-                </Card>
+                        <Card background='#8b9dc3'>
+                        <h3 style={{ textDecoration: 'underline' }}> Accepted Insurances </h3>
 
-                <Card background='#8b9dc3'>
-                <h3 style={{ textDecoration: 'underline' }}> Accepted Insurances </h3>
-
-                {data.insurances
-                  ? this.props.matches[0].insurances.map(insurance => insurance.insurance_provider.name).join(', ').slice(0,790)+"..."
-                  : <span>No insurance listed. Contact doctor for more information.</span>}
-               
-                </Card>
+                        {data.insurances
+                          ? this.props.matches[0].insurances.map(insurance => insurance.insurance_provider.name).join(', ').slice(0,790)+"..."
+                          : <span>No insurance listed. Contact doctor for more information.</span>}
+                      </Card>
                 </CardStack>    
-                </li>
-                </ul>  
-                </div>
-                
-                )
-
-              })
-                
-              : <h2 className="no-match">NO MATCHES!</h2>
-          }
+              </li>
+            </ul>  
+          </div>
+        )
+      }) : 
+          <h2 className="no-match">NO MATCHES!</h2>
+    }
         </div>
       </div>
     );
